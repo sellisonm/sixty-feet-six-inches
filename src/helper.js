af6e47cb4b250afcb3e781f97650f00a2d0bcb9c
Scott Ellison <sellison@gmail.com>
2021-11-25 09:51:59 -0500

    init players from setup

---
+++ src/helper.js
@@ -0,0 +1,18 @@
+
+
+function nameGenerator(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * 
+ charactersLength));
+   }
+   return result;
+}
+
+
+
+module.exports = {
+    nameGenerator
+}
--- src/setup.js
+++ src/setup.js
@@ -1,12 +1,18 @@
 const card = require('./card')
+const player = require('./player')
 
 let setupGame = () => {
 
     // 1. Load Cards From JSON data
     let cards = card.loadCardsFromData()
-    console.log(cards)
 
-    // 2. 
+    console.log('Cards: ')
+    cards.forEach(c => { console.log(c) });
+
+    // 2. Create Players
+    let players = player.initPlayers(2)
+    console.log('Players: ')    
+    players.forEach(p => { console.log(p) });
 
 }
 
